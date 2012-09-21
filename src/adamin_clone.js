83b02f050d110916868f5b2bf766bbebbffdd0cf
Adam L <git@adamin.net>
2012-09-21 12:40:43 -0700

    Initial commit

---
+++ src/adamin_clone.js
@@ -0,0 +1,28 @@
+/*
+ * adamin_clone
+ * https://github.com/pensive612/Adamin-Clone
+ *
+ * Copyright (c) 2012 Adam L.
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
